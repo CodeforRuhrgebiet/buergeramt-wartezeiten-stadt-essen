c8b4ecd7b2bdfe61ed5281cd2971fed57d81006b
Martin Schurig <martin@schurig.pw>
2016-06-09 21:44:40 +0200

    collect more data

--- lib/case.rb
+++ lib/case.rb
@@ -1,6 +1,7 @@
 class Case
   require 'open-uri'
   require 'nokogiri'
+  require 'date'
 
   @request_path = false
   @raw_response = false
@@ -16,18 +17,30 @@ class Case
   def data
     {
       beschreibung: @title,
-      anfrage_datum: Date.today,
+      anfrage_datum: Time.now.to_i,
       standort: @raw_details.xpath('//*[@id="standort"]').last['value'],
       wartebereich: @raw_details.xpath('//*[@id="warteschlange"]').last['value'],
       adresse: @raw_details.xpath('//*[@id="adresse"]').last['value'],
-      anliegen: @raw_details.xpath('//*[@id="anliegen"]').last['value'],
+      anliegen: @raw_details.xpath('//*[@id="anliegen"]').last.text,
       tag: @raw_details.xpath('//*[@id="tag"]').last['value'],
       uhrzeit: @raw_details.xpath('//*[@id="uhrzeit"]').last['value'],
-      wartezeit: 'x tage'
+      termin: appointment.to_i,
+      wartetage: waiting_days
     }
   end
 
   private
+
+  def appointment
+    date_string = @raw_details.xpath('//*[@id="tag"]').last['value'].match(/(0[1-9]|[1-2][0-9]|3[0-1]).(0[1-9]|1[0-2]).[0-9]{4}/).to_s
+    time_string = @raw_details.xpath('//*[@id="uhrzeit"]').last['value'].gsub(' Uhr', '')
+    DateTime.strptime("#{date_string}-#{time_string}", '%d.%m.%Y-%H:%M').to_time
+  end
+
+  def waiting_days
+    diff = appointment - Time.now
+    (diff / (60*60*24)).to_i
+  end
 
   def set_raw_response!
     @raw_response = Nokogiri::HTML(open("#{base_url}#{@request_path}"))
--- run.rb
+++ run.rb
@@ -5,11 +5,11 @@ Dir["#{project_root}/lib/*.rb"].each {|file| require file }
 perso_data = Case.new('Personalausweis beantragen', '?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=810&kuerzel=nPA&schlangen=2-31-34-20-18-25-5-23-36')
 p perso_data.data.inspect
 
+# Anmeldung in Essen
+anmelde_data = Case.new('Anmeldung in Essen', '?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=731&kuerzel=Anmeld&schlangen=2-31-34-20-18-25-5-23-36')
+p anmelde_data.data.inspect
 
-#
-# # Anmeldung in Essen
-# anmeldung_page = Nokogiri::HTML(open('https://meintermin.essen.de/termine/index.php?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=731&kuerzel=Anmeld&schlangen=2-31-34-20-18-25-5-23-36'))
-#
-# # Ummeldung innerhalb Essens (ohne KfZ)
-# ummeldung_page = Nokogiri::HTML(open('https://meintermin.essen.de/termine/index.php?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=1410&kuerzel=Umeldohne&schlangen=2-31-34-20-18-25-5-23-36'))
 
+# Ummeldung innerhalb Essens (ohne KfZ)
+ummeldung_data = Case.new('Ummeldung innerhalb Essens (ohne KfZ)', '?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=1410&kuerzel=Umeldohne&schlangen=2-31-34-20-18-25-5-23-36')
+p ummeldung_data.data.inspect
