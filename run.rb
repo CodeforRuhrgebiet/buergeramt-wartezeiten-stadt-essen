7669bc62fcf0c1d111a6ae630df3981decda81a1
Martin Schurig <martin@schurig.pw>
2016-06-13 15:46:33 +0200

    add reisepass data

--- run.rb
+++ run.rb
@@ -9,6 +9,10 @@ Dir["#{project_root}/lib/*.rb"].each {|file| require file }
 perso_data = Case.new('perso-beantragen', 'Personalausweis beantragen', '?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=810&kuerzel=nPA&schlangen=2-31-34-20-18-25-5-23-36')
 perso_data.save_data!
 
+# Reisepass beantragen
+reisepass_data = Case.new('reisepass-beantragen', 'Reisepass beantragen', '?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=811&kuerzel=RP&schlangen=34-2-31-20-18-25-5-36-23')
+reisepass_data.save_data!
+
 # Anmeldung in Essen
 anmelde_data = Case.new('anmeldung','Anmeldung in Essen', '?anr=21&anwendung=332&action=open&page=standortauswahl&tasks=731&kuerzel=Anmeld&schlangen=2-31-34-20-18-25-5-23-36')
 anmelde_data.save_data!
